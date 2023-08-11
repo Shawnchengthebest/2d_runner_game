1b1bef868c427a708c1d7ef4ed23d724bc096459
Shawn <shawnthebestt2bs@gmail.com>
2023-08-11 12:38:35 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+window.addEventListener('load', function(){
+    //canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+    //classes
+    class InputHandler {
+
+    }
+    class Projectile {
+
+    }
+    class Particle {
+        
+    }
+    class Player {
+        constructor(game) {
+            console.log(game);  
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update(){
+            this.y += this.speedY;
+        }
+        draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    class Enemy {
+
+    }
+    class layer {
+
+    }
+    class Background {
+
+    }
+    class UI {
+
+    }
+    class Game {
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.Player = new Player(this); 
+        }
+        update(){
+            this.player.update();
+        }
+        draw(){
+            this.player.draw();
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
+    //animate loop
+    function animate(){
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate); 
+    }
+    animate();
+});
+
