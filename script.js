584a991c0b2e24a19d9e93c026908f7e2798e5af
Shawn <shawnthebestt2bs@gmail.com>
2023-08-21 21:33:00 -0700

    cool counter

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ window.addEventListener('load', function(){
         constructor(game){
             this.game = game
             this.fontSize = 25;
-            this.fontFamily = 'Serif';
+            this.fontFamily = 'helvetica';
             this.color = 'white';
         }
         draw(context){
