56ee49b884c4380eb5bb53393c547c79e2fee25d
Shawn <shawnthebestt2bs@gmail.com>
2023-09-21 20:58:42 -0700

    Added debug controls (d)

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ window.addEventListener('load', function(){
                     this.game.keys.push(e.key);
                 } else if (e.key === ' ') {
                     this.game.player.shootTop();
+                } else if (e.key === 'd') {
+                    this.game.debug = !this.game.debug;
                 }
             });
             window.addEventListener('keyup', e =>{
@@ -95,8 +97,7 @@ window.addEventListener('load', function(){
             }
         }
         draw(context){
-            context.fillStyle = 'blue'
-            context.fillRect(this.x, this.y, this.width, this.height);
+            if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             this.projectiles.forEach(p => p.draw(context));
         }
@@ -252,6 +253,7 @@ window.addEventListener('load', function(){
             this.gameTime = 0;
             this.timerlimit = 5000;
             this.speed = 1;
+            this.debug = true;
         }
         update(deltaTime){
             // game timer
